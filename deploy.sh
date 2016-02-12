11d0b21bd85d795d0627916aa19a076328203aeb
Ilshidur <nicolas-coutin@outlook.com>
2016-02-12 03:09:31 +0100

    [DEPLOY] Fix the deploy process.

---
+++ deploy.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#
+# This script is executed with TravisCI (see .travis.yml file) on the server using SSH
+#
+# Required :
+# * pm2
+# * bower
+# * grunt
+#
+
+cd `dirname $0`
+
+if [ -e "package.tgz" ]; then
+	echo "Extracting package.tgz ..."
+	tar -xzf package.tgz
+	echo "Extracted package.tgz !"
+	echo "Removing package.tgz ..."
+	rm package.tgz
+	echo "Removed package.tgz !"
+else
+	echo "No package.tgz found !"
+fi
+
+# PM2
+echo "Cheking PM2 ..."
+if sudo npm list -g | grep pm2
+then
+	echo "PM2 already installed."
+else
+	echo "Installing PM2 ..."
+	sudo npm install pm2 -g
+	echo "Installed PM2 !"
+fi
+# Bower
+echo "Cheking Bower ..."
+if sudo npm list -g | grep bower
+then
+	echo "Bower already installed."
+else
+	echo "Installing Bower ..."
+	sudo npm install bower -g
+	echo "Installed Bower !"
+fi
+# Grunt-cli
+echo "Cheking Grunt ..."
+if sudo npm list -g | grep grunt-cli
+then
+	echo "Grunt already installed."
+else
+	echo "Installing Grunt ..."
+	sudo npm install grunt-cli -g
+	echo "Installed Grunt !"
+fi
+
+## Install dependencies
+echo "Installing dependencies ..."
+sudo npm install
+sudo bower install
+echo "Installed dependencies !"
+
+echo "Building ..."
+grunt build
+echo "Building : done !"
+
+## Run with pm2 (and Grunt via run.sh)
+echo "Running PM2 ..."
+sudo pm2 startOrRestart ecosystem.json5 --env production
+
+echo "Deployed successfully !"
