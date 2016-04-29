8924e932f80194c72da2a2962f2ebb0591840f80
Ilshidur <nicolas-coutin@outlook.com>
2016-04-29 21:03:28 +0200

    [ROUTE : about] Fix disposition

--- client/app/about/about.controller.js
+++ client/app/about/about.controller.js
@@ -2,4 +2,12 @@
 
 angular.module('nicolasCoutinFrApp')
   .controller('AboutCtrl', function ($scope) {
+
+    $scope.getAge = function () {
+      var birthday = new Date(1995, 1, 24); // 1 => February
+      var ageDifMs = Date.now() - birthday.getTime();
+      var ageDate = new Date(ageDifMs); // miliseconds from epoch
+      return Math.abs(ageDate.getUTCFullYear() - 1970);
+    };
+
   });
--- client/app/about/about.controller.spec.js
+++ client/app/about/about.controller.spec.js
@@ -15,7 +15,4 @@ describe('Controller: AboutCtrl', function () {
     });
   }));
 
-  it('should ...', function () {
-    // 1.should.equal(1);
-  });
 });
