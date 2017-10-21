e37b48ec06d2ab2d2dde1e73c6fedc2a7106e4a5
Ilshidur <ilshidur@gmail.com>
2017-10-21 14:25:57 +0200

    feat: react-snapshot (with error)

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+  <App />,
+  document.getElementById('root')
+);
 registerServiceWorker();
