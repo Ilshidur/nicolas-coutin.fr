56e2a280e3483e768640eb017f3013aaed4f6984
Nicolas Coutin <nicolas@dataxplorer.fr>
2017-10-26 12:06:18 +0200

    feat: Google Analytics tracking

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+ReactGA.initialize('UA-62226552-2', {
+  debug: process.env.NODE_ENV !== 'production'
+});
+ReactGA.set({ page: window.location.pathname + window.location.search });
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
