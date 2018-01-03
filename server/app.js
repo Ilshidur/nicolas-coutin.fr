854c1476222b4700cb65634e53c9202183a315cf
Nicolas Coutin <nicolas@dataxplorer.fr>
2018-01-03 10:10:41 +0100

    feat(server): logs

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const helmet = require('helmet');
+const morgan = require('morgan');
 const path = require('path');
 
 const config = require('./config');
@@ -13,6 +14,7 @@ const app = express();
 
 app.set('port', config.port);
 
+app.use(morgan(config.isProduction ? 'combined' : 'dev'));
 app.use(helmet());
 
 // Parse application/x-www-form-urlencoded
