7bacbf33a180ab5c92c3e0998ced209f81734313
Nicolas COUTIN <ncoutin@scalair.fr>
2019-03-26 16:55:41 +0100

    feat: new projects !

--- client/src/data.js
+++ client/src/data.js
@@ -3,11 +3,12 @@ import previewBlog from './img/preview-blog.jpg';
 import previewCssContext from './img/preview-css-contest.jpg';
 import previewPortfolioCineLC from './img/preview-portfoliocinelc.jpg';
 import previewPizzaPaiFid from './img/preview-pizzapai-fid.jpg';
+import previewSRS from './img/preview_srs.png';
 
 const projects = [{
   name: 'LoL Item Sets Generator',
   summary: 'Contenus téléchargeables pour le jeu League of Legends',
-  description: 'Outil qui propose du contenu téléchargeable pour le jeu vidéo League of Legends. Les contenus sont générés à partir de l\'API REST du jeu. Une application permet de mettre à jour ces données automatiquement et comptabilise plus d\'un million de téléchargements. Hébergé sur server dédié.',
+  description: 'Outil qui propose du contenu téléchargeable pour le jeu vidéo League of Legends. Les contenus sont générés à partir de l\'API REST du jeu. Une application permet de mettre à jour ces données automatiquement et comptabilise plus d\'un million de téléchargements.',
   img: previewLISG,
   link: 'https://lol-item-sets-generator.org',
   show: true
@@ -31,10 +32,10 @@ const projects = [{
   description: '',
   img: previewPortfolioCineLC,
   link: 'https://portfoliocinelc.fr',
-  show: true
+  show: false
 }, {
   name: 'Pizza Paï fidélité',
-  summary: 'Site fidélité de Pizza Paï (cadre professionnel).',
+  summary: 'Site fidélité de Pizza Paï (cadre professionnel)',
   description: '',
   img: previewPizzaPaiFid,
   link: 'https://espacefidelite.pizzapai.fr',
@@ -44,7 +45,14 @@ const projects = [{
   summary: 'Exposition de peintures de Marie-jo Coutin',
   description: '',
   img: null,
-  link: 'https://mjo-peintures.fr',
+  link: 'https://mjopeintures.com',
+  show: false
+}, {
+  name: 'Wenta.fr',
+  summary: 'Site vitrine commissioné par M. Wenta, traducteur',
+  description: '',
+  img: null,
+  link: 'https://wenta.fr',
   show: false
 }, {
   name: 'MinEevee\'s blog',
@@ -53,6 +61,13 @@ const projects = [{
   img: null,
   link: 'https://mineevee.com',
   show: false
+}, {
+  name: 'Statut des services Scalair',
+  summary: 'Listing et statuts des incidents/maintenances sur les services de Scalair (cadre professionnel)',
+  description: 'Refonte du site',
+  img: previewSRS,
+  link: 'https://status.scalair.fr',
+  show: true
 }];
 
 export { projects };
